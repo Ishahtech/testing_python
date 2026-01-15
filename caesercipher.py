ceaf6091d3d999082d3a783d65b4159c43cb331c
Aishat Salawudeen <salawudeenaishatolabisi@gmail.com>
2026-01-15 08:38:50 +0100

    feat: update caeser cipher app build

--- caesercipher.py
+++ caesercipher.py
@@ -1,2 +1,5 @@
-shift = 5
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
+shift = 5
+shifted_alphabet = alphabet[shift:] + alphabet[:shift]
+translation_table = str.maketrans(alphabet, shifted_alphabet)
+text = 'hello world'
